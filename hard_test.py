844aef9945e7358dc2600d841813a186748f16de
RichardGordon06 <87995654+RichardGordon06@users.noreply.github.com>
2021-08-01 17:10:16 +0300

    Add files via upload

---
+++ hard_test.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try:
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser=webdriver.Chrome()
+    browser.get(link)
+    button = browser.find_element(By.CLASS_NAME, "btn-primary")
+    assert button, "No button"
+finally:
+    time.sleep(3)
+    browser.quit()
+        
+
